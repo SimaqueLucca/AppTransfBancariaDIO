495bea36395bae24447d3b9598ba91e3d1edccb7
Luca Simaque <luccasimaque@yahoo.com.br>
2021-05-19 20:59:21 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TransfBancarias
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
